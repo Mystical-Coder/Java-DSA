8abda808815adee85bf10c558bb6c0853c0d7b58
Mystical-Coder <mysticalcoder192@gmail.com>
2024-04-22 19:22:07 +0530

    Print Number in increasing order

--- Recursion/PringIncNum.java
+++ Recursion/PringIncNum.java
@@ -1,10 +1,14 @@
 public class PringIncNum {
 
     public static void printNum(int n) {
-
+        if (n == 0) {
+            return;
+        }
+        printNum(n - 1);
+        System.out.println(n);
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        printNum(10);
     }
 }
