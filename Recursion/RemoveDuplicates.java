4d385e732833b90d14f54bc8030aab67269b641f
Mystical-Coder <mysticalcoder192@gmail.com>
2024-04-25 23:30:05 +0530

    Remove duplucates from string

---
+++ Recursion/RemoveDuplicates.java
@@ -0,0 +1,23 @@
+public class RemoveDuplicates {
+    
+    public static void removeDup(String str, int indx, StringBuilder newStr, boolean[] arr){
+        if(indx == str.length()){
+            System.out.println(newStr);
+            return;
+        }
+        char ch = str.charAt(indx);
+        if(arr[ch - 'a'] == true){
+            removeDup(str, indx + 1, newStr, arr);
+        }else{
+            arr[ch-'a']= true;
+            removeDup(str, indx+1, newStr.append(ch), arr);
+        }
+    }
+
+
+
+
+    public static void main(String[] args) {
+        removeDup("visssshhhhhuuuuu", 0, new StringBuilder(), new boolean[26]);
+    }
+}
