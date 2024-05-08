ed3d3f74885e40861c987b982f74bc940e66e19b
Mystical-Coder <mysticalcoder192@gmail.com>
2024-05-08 10:55:08 +0530

    Practice file

--- Notes/Practice.java
+++ Notes/Practice.java
@@ -4,40 +4,15 @@ import java.util.Stack;
 
 public class Practice {
 
-       public static int calculate(String s) {
-        Stack<Integer> stack = new Stack<>();
-        int n = s.length();
-        char sign = '+';
-        for(int i = 0; i < n; i++){
-            char ch = s.charAt(i);
-            if(Character.isDigit(ch)){
-                int val = 0;
-                while(i < n && Character.isDigit(s.charAt(i))){
-                    val = val * 10 + (s.charAt(i) - '0');
-                    i++;
-                }
-                i--;
-                if(sign == '+'){
-                    stack.push(val);
-                }else if(sign == '-' ){
-                    stack.push(-val);
-                }else if(sign == '*'){
-                    int a = stack.pop();
-                    stack.push(a*val);
-                }else if(sign == '/'){
-                    int a = stack.pop();
-                    stack.push(a/val);
-                }
-            }else if(ch != ' '){
-                sign = ch;
+        public static int missingNumber(int[] nums) {
+            int len = nums.length;
+            int sum = (len * (len - 1)) / 2;
+            for(int val : nums){
+                sum = sum - val;
             }
+            return sum;
         }
-        int sum = 0;
-        while(stack.size() > 0) sum += stack.pop();
-        return sum;
-    }
-
     public static void main(String[] args) {
-        System.out.println(calculate("3+2*2"));
+        System.out.println(missingNumber(new int[]{3, 0, 1}));
     }
 }
