8add830763badae448a488e2aff7671ac2c16811
Mystical-Coder <mysticalcoder192@gmail.com>
2024-05-11 12:07:16 +0530

    practice

--- Notes/Practice.java
+++ Notes/Practice.java
@@ -1,18 +1,10 @@
 package Notes;
 
-import java.util.Stack;
 
 public class Practice {
 
-        public static int missingNumber(int[] nums) {
-            int len = nums.length;
-            int sum = (len * (len - 1)) / 2;
-            for(int val : nums){
-                sum = sum - val;
-            }
-            return sum;
-        }
+        
     public static void main(String[] args) {
-        System.out.println(missingNumber(new int[]{3, 0, 1}));
+       
     }
 }
