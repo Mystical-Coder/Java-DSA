471f8e96dc06e8516cb9cccc14c45a2afd0c79cf
Mystical-Coder <mysticalcoder192@gmail.com>
2024-05-11 12:07:06 +0530

    Bubble sort : O(n^2)

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,24 @@
+package Sorting;
+
+public class BubbleSort {
+
+    public static void sort(int[] arr){
+        for(int turn = 0; turn < arr.length - 1; turn++){
+            for(int j = 0; j < arr.length - 1 - turn; j++){
+                if(arr[j] > arr[j + 1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 1, 3, 2};
+        sort(arr);
+        for(int val : arr){
+            System.out.print(val+" ");
+        }
+    }
+}
