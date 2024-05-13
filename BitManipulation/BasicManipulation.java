fbf875ddc373bb2d6003d321753cea5b20968b58
Mystical-Coder <mysticalcoder192@gmail.com>
2024-05-13 12:02:54 +0530

    All basic bit manipulation operation

---
+++ BitManipulation/BasicManipulation.java
@@ -0,0 +1,11 @@
+package BitManipulation;
+
+public class BasicManipulation {
+    public static void main(String[] args) {
+        System.out.println(5 & 4);
+        System.out.println(5 | 4);
+        System.out.println(~(0));
+        System.out.println(5 << 2); // a << b = a * 2 ^ b
+        System.out.println(6 >> 1); // a >> b = a / 2^b
+    }
+}
