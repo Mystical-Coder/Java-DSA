dba94b56150bdd1f2879560fdca8eebe027ac7f7
Mystical-Coder <mysticalcoder192@gmail.com>
2024-05-16 16:46:57 +0530

    Valid parentheses

---
+++ StackOp/ValidParentheses.java
@@ -0,0 +1,41 @@
+package StackOp;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    public static boolean isValid(String str){
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
+            if(ch == '(' || ch == '{' || ch == '['){
+                stack.push(ch);
+            }else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                if((ch == ')' && stack.peek()== '(') 
+                || (ch == '}' && stack.peek() == '{') 
+                || (ch == ']' && stack.peek() == '[')){
+                    stack.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+        if(!stack.isEmpty()){
+            return false;
+        }
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        String str = "[][][][[[]]]";
+        if(isValid(str)){
+            System.out.println("Parentheses is valid");
+        }else{
+            System.out.println("Not valid");
+        }
+    }
+}
