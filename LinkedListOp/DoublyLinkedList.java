260d167959b27239bb6c6225a1fadb8424be5ff6
Mystical-Coder <mysticalcoder192@gmail.com>
2024-05-16 11:33:49 +0530

    remove last and add first in doubly linked list

--- LinkedListOp/DoublyLinkedList.java
+++ LinkedListOp/DoublyLinkedList.java
@@ -3,27 +3,26 @@ package LinkedListOp;
 public class DoublyLinkedList {
 
     public class Node {
-        
+
         int data;
         Node prev;
         Node next;
 
-        public Node(int data){
+        public Node(int data) {
             this.data = data;
             this.prev = null;
             this.next = null;
         }
     }
 
-    
     public static Node head;
     public static Node tail;
     public static int size;
 
-    public void addFirst(int data){
+    public void addFirst(int data) {
         Node newNode = new Node(data);
         size++;
-        if(head == null){
+        if (head == null) {
             head = tail = newNode;
             return;
         }
@@ -32,12 +31,12 @@ public class DoublyLinkedList {
         head = newNode;
     }
 
-    public int removeFirst(){
-        if(head == null){
+    public int removeFirst() {
+        if (head == null) {
             System.out.println("Linked list is empty");
             return Integer.MIN_VALUE;
         }
-        if(size == 1){
+        if (size == 1) {
             head = tail = null;
             size--;
         }
@@ -48,27 +47,52 @@ public class DoublyLinkedList {
         return val;
     }
 
-    public void printLL(){
+    public void addLast(int val) {
+        Node node = new Node(val);
+        size++;
+        if (head == null) {
+            head = tail = node;
+            return;
+        }
+        tail.next = node;
+        node.prev = tail;
+        tail = node;
+    }
+
+    public int removeLast() {
+        if (size == 1) {
+            head = tail = null;
+            size--;
+            return Integer.MIN_VALUE;
+        }
+        int val = tail.data;
+        tail = tail.prev;
+        tail.next = null;
+        size--;
+        return val;
+    }
+
+    public void printLL() {
         Node temp = head;
-        while(temp != null){
-            System.out.print(temp.data+"<->");
+        while (temp != null) {
+            System.out.print(temp.data + "<->");
             temp = temp.next;
         }
         System.out.println("null");
     }
 
-    public void reverseLL(){
+    public void reverseLL() {
         Node curr = head;
         Node prev = null;
         Node next;
-        while(curr != null){
+        while (curr != null) {
             next = curr.next;
             curr.next = prev;
             curr.prev = next;
             prev = curr;
-            curr =next;
+            curr = next;
         }
-       head = prev;
+        head = prev;
     }
 
     public static void main(String[] args) {
@@ -76,8 +100,12 @@ public class DoublyLinkedList {
         ll.addFirst(3);
         ll.addFirst(2);
         ll.addFirst(1);
+        ll.addLast(4);
+        ll.addLast(10);
         ll.printLL();
-        ll.reverseLL();
+        ll.removeLast();
+        ll.removeLast();
         ll.printLL();
+        System.out.println(ll.size);
     }
 }
