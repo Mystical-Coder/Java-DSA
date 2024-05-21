8d64444ee36e6adac97fa0b73b8f5fcb31765131
Mystical-Coder <mysticalcoder192@gmail.com>
2024-05-21 12:01:52 +0530

    Activity selection problem

---
+++ GreedyAlgo/ActivitySelection.java
@@ -0,0 +1,25 @@
+package GreedyAlgo;
+
+import java.util.ArrayList;
+
+public class ActivitySelection {
+
+    public static void main(String[] args) {
+        int start[] = {1, 3, 0, 5, 8, 5};
+        int[] end = {2, 4, 6, 7, 9, 9};
+        ArrayList<Integer> list = new ArrayList<>();
+        int maxAct = 1;
+        list.add(0);
+        int lastEnd = end[0];
+
+        for(int i = 1; i < end.length; i++){
+            if(start[i] >= lastEnd){
+                maxAct++;
+                list.add(i);
+                lastEnd = end[i];
+            }
+        }
+        System.out.println(maxAct);
+        System.out.println(list);
+    }
+}
