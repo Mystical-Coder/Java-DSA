523be164f1668fd09afc2c0f9456b989759737c3
Mystical-Coder <mysticalcoder192@gmail.com>
2024-05-23 11:05:20 +0530

    Minimum absolute difference

---
+++ GreedyAlgo/MinAbsDiff.java
@@ -0,0 +1,17 @@
+package GreedyAlgo;
+
+import java.util.Arrays;
+
+public class MinAbsDiff {
+    public static void main(String[] args) {
+        int[] a = {4, 1, 8, 7};
+        int[] b = {2, 3, 6, 5};
+        Arrays.sort(a);
+        Arrays.sort(b);
+        int minSum = 0;
+        for(int i = 0; i < a.length; i++){
+            minSum += Math.abs(a[i] - b[i]);
+        }
+        System.out.println("Maximum sum is: "+minSum);
+    }
+}
