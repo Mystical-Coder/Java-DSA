030e99735a22a72863cf2abfdec3fc508fe027aa
Mystical-Coder <mysticalcoder192@gmail.com>
2024-05-30 13:26:06 +0530

    Binary tree diameter

---
+++ BinaryTreeOp/BTDiameter.java
@@ -0,0 +1,69 @@
+package BinaryTreeOp;
+
+public class BTDiameter {
+
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+        public Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static int height(Node root){
+        if(root == null){
+            return 0;
+        }
+        int lh = height(root.left);
+        int rh = height(root.right);
+        return Math.max(lh, rh) + 1;
+    }
+
+    public static int diameter1(Node root){ // O(n^2)
+        if(root == null){
+            return 0;
+        }
+        int leftDiam = diameter1(root.left);
+        int lh = height(root.left);
+        int rightDiam = diameter1(root.right);
+        int rh = height(root.right);
+        int self = lh + rh + 1;
+        return Math.max(self, Math.max(leftDiam, rightDiam));
+    }
+
+    public static class Info {
+        int diam;
+        int ht;
+        public Info(int diam, int ht){
+            this.diam = diam;
+            this.ht = ht;
+        }
+    }
+
+    public static Info diameter2(Node root){ //O(n)
+        if(root == null){
+            return new Info(0, 0);
+        }
+        Info leftInfo = diameter2(root.left);
+        Info rightInfo = diameter2(root.right);
+        int diam = Math.max(Math.max(leftInfo.diam, rightInfo.diam), leftInfo.ht + rightInfo.ht + 1);
+        int ht = Math.max(leftInfo.ht, rightInfo.ht) + 1;
+        return new Info(diam, ht);
+    } 
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        // System.out.println(height(root));
+        // System.out.println(diameter1(root));
+        System.out.println(diameter2(root).diam);
+    }
+}
