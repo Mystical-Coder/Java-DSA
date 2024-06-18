ccb754c937a7f1dd812d7068fea62ff238645819
Mystical-Coder <mysticalcoder192@gmail.com>
2024-06-18 12:07:34 +0530

    Count unique substring

---
+++ TrieOp/CountUniqueSubstring.java
@@ -0,0 +1,50 @@
+package TrieOp;
+
+public class CountUniqueSubstring {
+
+    static class Node{
+        Node children[] = new Node[26];
+        boolean eow = false;
+        public Node(){
+            for(int i = 0; i < 26; i++){
+                children[i] = null;
+            }
+        }
+    }
+
+    public static Node root = new Node();
+
+    public static void insert(String word){ // O(L), L: length of the largest word
+        Node curr = root;
+        for(int level = 0; level < word.length(); level++){
+            int idx = word.charAt(level) - 'A';
+            if(curr.children[idx] == null){
+                curr.children[idx] = new Node();  
+            }
+            curr=  curr.children[idx];
+        }
+        curr.eow = true;
+    }
+
+    public static int countNodes(Node root){
+        if(root == null){
+            return 0;
+        }
+        int count = 0;
+        for(int i = 0; i < 26; i++){
+            if(root.children[i] != null){
+                count += countNodes(root.children[i]);
+            }
+        }
+        return count + 1;
+    }
+
+    public static void main(String[] args) {
+        // String str = "ababa"; //10
+        String str = "LEETCODE"; //15
+        for(int i = 0; i < str.length(); i++){
+            insert(str.substring(i));
+        }
+        System.out.println(countNodes(root));
+    }
+}
