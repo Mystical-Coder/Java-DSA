3e939bbcfcad5fc06b477af48ffea637433209da
Mystical-Coder <mysticalcoder192@gmail.com>
2024-06-25 11:58:20 +0530

    Strongly connected component

---
+++ Graph/StronglyConnectedComp.java
@@ -0,0 +1,35 @@
+package Graph;
+
+import java.util.ArrayList;
+
+public class StronglyConnectedComp {
+
+    public static class Edge {
+        int src;
+        int dest;
+
+        public Edge(int src, int dest) {
+            this.src = src;
+            this.dest = dest;
+        }
+    }
+
+    public static void createGraph(ArrayList<Edge>[] graph) {
+        for(int i = 0; i < graph.length; i++){
+            graph[i] = new ArrayList<>();
+        }
+        graph[0].add(new Edge(0, 2));
+        graph[0].add(new Edge(0, 3));
+
+        graph[1].add(new Edge(1, 0));
+
+        graph[2].add(new Edge(2, 1));
+        
+        graph[3].add(new Edge(3, 4));
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
