b1fe3266d2fc3e15218c6b4e1c0e29baff029868
Mystical-Coder <mysticalcoder192@gmail.com>
2024-07-12 15:28:31 +0530

    Longest common subsequence

---
+++ DP/LongestCommonSubstring.java
@@ -0,0 +1,33 @@
+package DP;
+
+public class LongestCommonSubstring {
+
+    public static int getLCS(String str1, String str2) {
+        int[][] dp = new int[str1.length()][str2.length()];
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        int ans = Integer.MIN_VALUE;
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    ans = Math.max(dp[i][j], ans);
+                } else {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        String str1 = "ABCDE";
+        String str2 = "ABGCE";
+        System.out.println(getLCS(str1, str2));
+    }
+}
