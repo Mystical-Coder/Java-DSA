b336dca95d4e62e91b05893260262d8ae33e8312
Mystical-Coder <mysticalcoder192@gmail.com>
2024-07-16 12:09:26 +0530

    catalans number

--- DP/CatalansNum.java
+++ DP/CatalansNum.java
@@ -1,5 +1,7 @@
 package DP;
 
+import java.util.Arrays;
+
 public class CatalansNum {
 
     public static int getCat(int n) {
@@ -8,13 +10,45 @@ public class CatalansNum {
         }
         int ans = 0;
         for (int i = 0; i <= n - 1; i++) {
-            ans += getCat(n) * getCat(n - i - 1);
+            ans += getCat(i) * getCat(n - i - 1);
         }
         return ans;
     }
 
+    public static int getCatMemoization(int n, int[] dp) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+        int ans = 0;
+        for (int i = 0; i <= n - 1; i++) {
+            ans += getCat(i) * getCat(n - i - 1);
+        }
+        return dp[n] = ans; 
+    }
+
+    public static int getCatTabulation(int n){
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i <=n; i++){
+            for(int j = 0; j < i; j++){
+                dp[i] += dp[j] * dp[i - j - 1];
+            }
+        }
+        return dp[n];
+    }
+
     public static void main(String[] args) {
-        int n = 4;
-        System.out.println(getCat(n));
+        int n = 4; // 14
+        // int n = 5; // 42
+        // int n = 30; // Take alot of time
+        int[] dp = new int[n + 1];
+        // Arrays.fill(dp, -1);
+        // System.out.println(getCat(n));
+        // System.out.println(getCatMemoization(n, dp));
+        System.out.println(getCatTabulation(n));
     }
 }
