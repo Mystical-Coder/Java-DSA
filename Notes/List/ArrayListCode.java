8b2473743a9020583fd72e156d12dc26bb605e54
Mystical-Coder <mysticalcoder192@gmail.com>
2025-03-25 13:25:35 +0530

    ListNotes added

---
+++ Notes/List/ArrayListCode.java
@@ -0,0 +1,19 @@
+package Notes.List;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArrayListCode {
+    public static void main(String[] args) {
+    }
+    
+    public static void addInList(){
+        
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        list.add(new ArrayList<>(Arrays.asList(1, 2,4)));
+
+        
+
+    }
+
+}
