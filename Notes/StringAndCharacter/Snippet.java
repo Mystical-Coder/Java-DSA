2e705f9b8254ebd502c6e7ce75d6e6315bde1518
Mystical-Coder <mysticalcoder192@gmail.com>
2025-03-27 16:15:54 +0530

    Code snippet of string and character is added

---
+++ Notes/StringAndCharacter/Snippet.java
@@ -0,0 +1,21 @@
+package Notes.StringAndCharacter;
+
+public class Snippet {
+
+
+    public static boolean letterOrDigit(char ch){
+        return Character.isLetterOrDigit(ch);
+    }
+    
+    public static void stringBuidlerCode(){
+        StringBuilder str = new StringBuilder();
+        str.append("value");
+    }
+    
+    
+    public static void main(String[] args) {
+
+    }
+
+
+}
