a59a148132be0635c68e610f7747f3dba257f0bc
Mystical-Coder <mysticalcoder192@gmail.com>
2025-03-29 16:11:27 +0530

    String snippet added

--- Notes/StringAndCharacter/Snippet.java
+++ Notes/StringAndCharacter/Snippet.java
@@ -1,5 +1,7 @@
 package Notes.StringAndCharacter;
 
+import java.util.Stack;
+
 public class Snippet {
 
 
@@ -9,12 +11,18 @@ public class Snippet {
     
     public static void stringBuidlerCode(){
         StringBuilder str = new StringBuilder();
-        str.append("value");
+        // str.append("value");
+        String str1 = "value";
+        String str2 = "value2";
+        str.append(str1);
     }
     
     
     public static void main(String[] args) {
-
+        Stack<String> stack = new Stack<>();
+        String exp = "Value";
+        String ch = exp.charAt(1)+"";
+        
     }
 
 
