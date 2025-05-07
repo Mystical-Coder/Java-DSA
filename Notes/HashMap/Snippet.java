2b9e819fa19784db27d7e91ade0e998d0dd83400
Mystical-Coder <mysticalcoder192@gmail.com>
2025-05-07 16:11:30 +0530

    hashmap function updated

---
+++ Notes/HashMap/Snippet.java
@@ -0,0 +1,14 @@
+package Notes.HashMap;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.List;
+
+public class Snippet {
+    public static void main(String[] args) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        HashMap<Integer, List<Integer>> dmap = new HashMap<>();
+        Deque<Integer> deque = new ArrayDeque<>();
+    }
+}
